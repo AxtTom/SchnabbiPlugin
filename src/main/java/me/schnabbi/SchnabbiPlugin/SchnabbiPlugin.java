4299eada01be7b6c272a6607af8ea41897b98e34
AxtTom <wolftom4321@gmail.com>
2022-09-19 19:38:34 +0200

    first commit

---
+++ src/main/java/me/schnabbi/SchnabbiPlugin/SchnabbiPlugin.java
@@ -0,0 +1,11 @@
+package me.schnabbi.SchnabbiPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class SchnabbiPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("nigga");
+    }
+}
